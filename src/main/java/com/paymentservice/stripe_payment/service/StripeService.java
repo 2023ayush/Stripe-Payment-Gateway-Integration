c6bbe8b98043ec84980390b0c2a03bf3b51e84dd
Your GitHub Username <your-email@example.com>
2025-06-04 22:48:58 +0530

    Integrate Stripe payment gateway with session-based checkout

--- src/main/java/com/paymentservice/stripe_payment/service/StripeService.java
+++ src/main/java/com/paymentservice/stripe_payment/service/StripeService.java
@@ -1,2 +1,71 @@
-package com.paymentservice.stripe_payment.service;public class StripeService {
+package com.paymentservice.stripe_payment.service;
+
+
+
+
+import com.paymentservice.stripe_payment.dto.ProductRequest;
+import com.paymentservice.stripe_payment.dto.StripeResponse;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class StripeService {
+
+    @Value("${stripe.secretKey}")
+    private String secretKey;
+
+    public StripeResponse checkoutProducts(ProductRequest productRequest) {
+
+        Stripe.apiKey = secretKey;
+
+        SessionCreateParams.LineItem.PriceData.ProductData productData =
+                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                        .setName(productRequest.getName())
+                        .build();
+
+
+        SessionCreateParams.LineItem.PriceData priceData =
+                SessionCreateParams.LineItem.PriceData.builder()
+                        .setCurrency(productRequest.getCurrency() != null ? productRequest.getCurrency() : "USD")
+                        .setUnitAmount(productRequest.getAmount())
+                        .setProductData(productData)
+                        .build();
+
+
+        SessionCreateParams.LineItem lineItem =
+                SessionCreateParams
+                        .LineItem.builder()
+                        .setQuantity(productRequest.getQuantity())
+                        .setPriceData(priceData)
+                        .build();
+
+
+        SessionCreateParams params =
+                SessionCreateParams.builder()
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setSuccessUrl("http://localhost:8080/success")
+                        .setCancelUrl("http://localhost:8080/cancel")
+                        .addLineItem(lineItem)
+                        .build();
+
+        Session session = null;
+        try {
+            session = Session.create(params);
+        } catch (StripeException e) {
+
+        }
+
+        return StripeResponse
+                .builder()
+                .status("SUCCESS")
+                .message("Payment session created ")
+                .sessionId(session.getId())
+                .sessionUrl(session.getUrl())
+                .build();
+    }
 }
